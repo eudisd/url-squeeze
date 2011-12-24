6a118e59a52a26ab9b9180651463e94e33b41fb1
Eudis Duran <eudisd@live.com>
2011-12-24 13:35:16 -0500

    Commiting some errors...

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 #! /usr/bin/env python
 
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+
+class MainPage(webapp.ResponseHandler):
+    def get() 
 
 def main():
+    """application = 
+    run_wsgi_app(application)
+    """
     return
-    
   
 if __name__ == "__main__":
     main()

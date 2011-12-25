1719495f01f038beac58b0288e0d798170ba6571
Eudis Duran <eudisd@live.com>
2011-12-25 15:39:48 -0500

    Didi the string replacement on the default input url box

--- js/start.js
+++ js/start.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     $("#tech").facebox();
+    
+    $("#url_input").live('click', function(){
+        $("#url_input").val('');
+    });
+    
+    $("#url_input").live('blur', function() {
+        var val = $("#url_input").val();
+        if( val == '' || val == ' ' ){
+            $("#url_input").val("Long URL goes here!");
+        }   
+    });
 });

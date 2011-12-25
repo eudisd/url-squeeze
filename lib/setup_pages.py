6f0d754effc92eb5357649f1086243ab5d9baee7
Eudis Duran <eudisd@live.com>
2011-12-25 13:22:30 -0500

    ...

---
+++ lib/setup_pages.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python 
+
+import os
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        path = os.path.join(os.path.dirname(__file__), '../templates/index.html')
+        self.response.out.write(template.render(path, {}))
--- main.py
+++ main.py
@@ -2,15 +2,16 @@
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.ext.webapp import template
-
-class MainPage(webapp.ResponseHandler):
-    def get() 
+import lib.setup_pages
 
 def main():
-    """application = 
+    application = webapp.WSGIApplication(
+    [
+    ('/', lib.setup_pages.MainPage)
+    ('/stats', lib.setup_pages.StatsPage)
+    ], debug=True)
+    
     run_wsgi_app(application)
-    """
     return
   
 if __name__ == "__main__":
